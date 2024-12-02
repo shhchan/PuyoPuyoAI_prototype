fcba55453ad1d24e02dc4db8321545b89d1ef5ea
shhchan <sion2000114@gmail.com>
2024-12-03 01:46:43 +0900

    create `enum Color`.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
 ﻿#include <iostream>
 
-void put_color(int c) {
+enum Color {
+  RED = 41,
+};
+
+void put_color(Color c) {
   std::cout << "\033[" << c << "m  \033[0m";
 }
 
 int main()
 {
-  put_color(41);
+  put_color(Color::RED);
 }
