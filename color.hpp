fd4af1d9a2317de4edc217e5628dcf838d9c5c68
shhchan <sion2000114@gmail.com>
2024-12-04 00:50:53 +0900

    fix space.

--- color.hpp
+++ color.hpp
@@ -2,14 +2,14 @@
 #define COLOR_HPP
 
 enum Color {
-	EMPTY = 40,		// 黒
-	RED = 41,		// 赤
-	BLUE = 44,		// 青
-	YELLOW = 43,		// 黄
-	GREEN = 42,		// 緑
-	PURPLE = 45,		// 紫
-	WASTED = 100,	// おじゃま
-	COLOR_MAX = 8			// 色の数をカウント
+	EMPTY = 40, // 黒
+	RED = 41, // 赤
+	BLUE = 44, // 青
+	YELLOW = 43, // 黄
+	GREEN = 42, // 緑
+	PURPLE = 45, // 紫
+	WASTED = 100, // おじゃま
+	COLOR_MAX = 8 // 色の数をカウント
 };
 
 void put_color(Color);
