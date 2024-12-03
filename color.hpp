c5819f746919b5d55f476401a180cec0a6cafc8f
shhchan <sion2000114@gmail.com>
2024-12-04 00:49:14 +0900

    fix space.

--- color.hpp
+++ color.hpp
@@ -2,13 +2,13 @@
 #define COLOR_HPP
 
 enum Color {
-	EMPTY			= 40,		// 黒
-	RED				= 41,		// 赤
-	BLUE			= 44,		// 青
-	YELLOW		= 43,		// 黄
-	GREEN			= 42,		// 緑
-	PURPLE		= 45,		// 紫
-	WASTED		= 100,	// おじゃま
+	EMPTY = 40,		// 黒
+	RED	= 41,		// 赤
+	BLUE = 44,		// 青
+	YELLOW = 43,		// 黄
+	GREEN = 42,		// 緑
+	PURPLE = 45,		// 紫
+	WASTED = 100,	// おじゃま
 	COLOR_MAX = 8			// 色の数をカウント
 };
 
